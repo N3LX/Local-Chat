fe99f0b45aafcbee280cf5f50faa04c2fe528032
Wojtek Hołowiak <w.holowiak@gmail.com>
2022-03-30 23:54:01 +0200

    Created server-side code to the point where creation of client-side code is required to progress further.

---
+++ src/main/java/com/n3lx/chat/server/Server.java
@@ -0,0 +1,132 @@
+package com.n3lx.chat.server;
+
+import com.n3lx.chat.Message;
+import com.n3lx.chat.util.Settings;
+import javafx.scene.control.ListView;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Server {
+
+    private static final Logger LOGGER = Logger.getLogger(Server.class.getName());
+
+    private final String serverName;
+    private final String userName;
+    private final String welcomeMessage;
+    private final ListView<String> chatBox;
+    private final ListView<String> userListBox;
+
+    private ServerSocket serverSocket;
+    private List<ClientStream> clients;
+    private ExecutorService serverThreads;
+
+    public Server(String serverName, String userName, String welcomeMessage, ListView<String> chatBox, ListView<String> userList) {
+        this.serverName = serverName;
+        this.userName = userName;
+        this.welcomeMessage = welcomeMessage;
+        this.chatBox = chatBox;
+        this.userListBox = userList;
+    }
+
+    public void start() {
+        try {
+            serverSocket = new ServerSocket(Settings.PORT);
+            serverThreads = Executors.newCachedThreadPool();
+            clients = new LinkedList<>();
+
+            startIncomingConnectionHandler();
+            startMessageHandler();
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "An error has occurred during server startup", e);
+        }
+    }
+
+    public void stop() {
+        try {
+            serverThreads.shutdown();
+            serverSocket.close();
+
+            while (!serverThreads.isTerminated()) {
+                Thread.sleep(10);
+            }
+
+            for (ClientStream client : clients) {
+                client.close();
+            }
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "An error has occurred during server closure", e.toString());
+        } catch (InterruptedException ignored) {
+
+        }
+    }
+
+    private void startIncomingConnectionHandler() {
+        /*
+        A runnable that will process all incoming connections
+        */
+        Runnable incomingConnectionHandler = () -> {
+            while (!serverSocket.isClosed()) {
+                try {
+                    Socket clientSocket = serverSocket.accept();
+                    ClientStream clientStream = new ClientStream(clientSocket);
+
+                    //TODO Implement handshake
+
+                    synchronized (clients) {
+                        clients.add(clientStream);
+                    }
+                } catch (IOException ignored) {
+
+                }
+            }
+        };
+        serverThreads.submit(incomingConnectionHandler);
+    }
+
+    private void startMessageHandler() {
+        Runnable messageHandler = () -> {
+            while (!serverSocket.isClosed()) {
+                synchronized (clients) {
+                    for (ClientStream client : clients) {
+                        try {
+                            Message message = (Message) client.getObjectInputStream().readObject();
+
+                            switch (message.getMessageType()) {
+                                case STANDARD:
+                                    for (ClientStream recipient : clients) {
+                                        recipient.getObjectOutputStream().writeObject(message);
+                                    }
+                                    appendMessageToChatBox(message);
+                                    break;
+                                case ACTION:
+                                    //TODO Implement special message handling
+                                    break;
+                            }
+                        } catch (SocketTimeoutException ignored) {
+                            //No activity on the socket, can proceed further
+                        } catch (ClassNotFoundException | IOException e) {
+                            LOGGER.log(Level.WARNING, "Could not parse incoming message", e);
+                        }
+                    }
+                }
+            }
+        };
+        serverThreads.submit(messageHandler);
+    }
+
+    private void appendMessageToChatBox(Message message) {
+        StringBuilder msg = new StringBuilder();
+        msg.append(message.getUsername()).append(": ").append(message.getMessage());
+        chatBox.getItems().add(msg.toString());
+    }
+
+}
---
+++ src/main/java/com/n3lx/chat/util/Settings.java
@@ -0,0 +1,7 @@
+package com.n3lx.chat.util;
+
+public class Settings {
+
+    public static final int PORT = 5005;
+
+}
